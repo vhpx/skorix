57b6c2abef48bf5fc38403a2af210c3b0ef08d16
s3927562 <s3927562@rmit.edu.vn>
2024-05-13 18:15:25 +0700

    Interrupt and Timer
    - Added code to turn Pin 5/GPIO 3 on and off

--- src/lib/utils/timer.c
+++ src/lib/utils/timer.c
@@ -1,10 +1,14 @@
 #include "../headers/timer.h"
 #include "../headers/uart0.h"
+#include "../headers/gpio.h"
 
 unsigned int timer1_cmp = 0;
 unsigned int timer1_period = TIMER_CLOCK;
 
 void sys_timer1_init(void) {
+    // Set GPIO3 as output
+    GPFSEL0 |= 1 << 9;
+
     timer1_cmp = TIMER_CLO + timer1_period;
     TIMER_C1 = timer1_cmp;
     TIMER_CS |= TIMER_CS_M1;
@@ -15,6 +19,10 @@ void handle_sys_timer1(void) {
     TIMER_C1 = timer1_cmp;
     TIMER_CS |= TIMER_CS_M1;
 
-    // Function here
-    // uart_puts("Timer 1 interrupt\n");
+    // Turn GPIO3 on and off
+    if (GPLEV0 & (1 << 3)) {
+        GPCLR0 |= 1 << 3;
+    } else {
+        GPSET0 |= 1 << 3;
+    }
 }

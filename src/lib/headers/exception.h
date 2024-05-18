354f9733d705aca4f142cbaeed984aa150f73a04
s3927562 <s3927562@rmit.edu.vn>
2024-05-18 15:24:15 +0700

    Update show_exception

--- src/lib/headers/exception.h
+++ src/lib/headers/exception.h
@@ -28,6 +28,6 @@ void exception_init(void);
 int get_el(void);
 
 // C functions
-void show_exception(unsigned int type, unsigned long esr, unsigned long far, unsigned long elr);
+void show_exception(unsigned int type, unsigned long esr, unsigned long address);
 
 #endif
--- src/lib/utils/exception.c
+++ src/lib/utils/exception.c
@@ -1,18 +1,14 @@
 #include "../headers/exception.h"
 #include "../headers/uart0.h"
 
-void show_exception(unsigned int type, unsigned long esr, unsigned long far, unsigned long elr) {
+void show_exception(unsigned int type, unsigned long esr, unsigned long address) {
     uart_puts("\n\nSomething went wrong...\n");
-    uart_puts("Exception type: ");
+    uart_puts("Exception: ");
     uart_puts(exceptions[type]);
-    uart_puts("\n");
-    uart_puts("ESR: ");
+    uart_puts(" (");
     uart_hex(esr);
-    uart_puts("\n");
-    uart_puts("FAR: ");
-    uart_hex(far);
-    uart_puts("\n");
-    uart_puts("ELR: ");
-    uart_hex(elr);
+    uart_puts(")\n");
+    uart_puts("Address: ");
+    uart_hex(address);
     uart_puts("\n");
 }

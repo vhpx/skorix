5e8a279eef95bb16b5c491e9a66577ba2438f149
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-05-23 21:10:49 +0700

    feat!: update wait_msec logic

--- src/lib/utils/utils.c
+++ src/lib/utils/utils.c
@@ -18,17 +18,20 @@ unsigned char getUart() {
 
 /* Functions to delay, set/wait timer */
 
-void wait_msec(unsigned int n) {
-  register unsigned long f, t, r, expiredTime;
+void wait_msec(unsigned int msVal) {
+  register unsigned long f, t, r, expiredTime; // 64 bits
 
-  // Get the current counter frequency (Hz)
+  // Get the current counter frequency (Hz), 1Hz = 1 pulses/second
   asm volatile("mrs %0, cntfrq_el0" : "=r"(f));
 
   // Read the current counter value
   asm volatile("mrs %0, cntpct_el0" : "=r"(t));
 
   // Calculate expire value for counter
-  expiredTime = t + ((f / 1000) * n) / 1000;
+  /* Note: both expiredTime and counter value t are 64 bits,
+  thus, it will still be correct when the counter is overflow */
+  expiredTime = t + f * msVal / 1000;
+
   do {
     asm volatile("mrs %0, cntpct_el0" : "=r"(r));
   } while (r < expiredTime);
--- src/video/video.c
+++ src/video/video.c
@@ -1,12 +1,13 @@
 // this file is video.c
 #include "video.h"
 #include "../img/img.h"
+#include "../lib/headers/cli.h"
 #include "../lib/headers/constants.h"
 #include "../lib/headers/framebf.h"
+#include "../lib/headers/interrupt.h"
 #include "../lib/headers/utils.h"
 #include "video_src.h"
-#include "../lib/headers/interrupt.h"
-#include "../lib/headers/cli.h"
+
 
 int video_pause = 0;
 int video_exit = 0;
@@ -14,33 +15,33 @@ int video_end = 1;
 int video_restart = 0;
 
 void display_video(int video_width, int video_height) {
-    video_pause = 0;
-    video_exit = 0;
-    video_end = 0;
-    video_restart = 0;
-    interrupt_enable();
+  video_pause = 0;
+  video_exit = 0;
+  video_end = 0;
+  video_restart = 0;
+  interrupt_enable();
 
-    // Loop through each frame stored in the array
-    for (int i = 0; i < video_bitmap_allArray_LEN; i++) {
-        draw_image(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, video_bitmap_allArray[i]);
-        wait_msec(100000);
+  // Loop through each frame stored in the array
+  for (int i = 0; i < video_bitmap_allArray_LEN; i++) {
+    draw_image(0, 0, VIDEO_WIDTH, VIDEO_HEIGHT, video_bitmap_allArray[i]);
+    wait_msec(100);
 
-        if (video_pause) {
-            i--;
-        }
-
-        if (video_exit) {
-            break;
-        }
-
-        if (video_restart) {
-            i = 0;
-            video_pause = 0;
-            video_restart = 0;
-        }
+    if (video_pause) {
+      i--;
     }
 
-    interrupt_disable();
-    clear_frame_buffer(SCREEN_WIDTH, SCREEN_HEIGHT);
-    video_end = 1;
+    if (video_exit) {
+      break;
+    }
+
+    if (video_restart) {
+      i = 0;
+      video_pause = 0;
+      video_restart = 0;
+    }
+  }
+
+  interrupt_disable();
+  clear_frame_buffer(SCREEN_WIDTH, SCREEN_HEIGHT);
+  video_end = 1;
 }

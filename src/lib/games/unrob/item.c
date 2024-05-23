79576ed8f8d02048f21a770a95451abe9fcc9c81
Võ Hoàng Phúc <contact@vohoangphuc.com>
2024-05-23 23:14:57 +0700

    chore: consolidate gengine object headers

--- src/lib/games/unrob/item.c
+++
@@ -1,9 +0,0 @@
-#include "../../headers/constants.h"
-#include "../../headers/gengine-core.h"
-
-typedef struct {
-  int id;
-  char name[MAX_GENGINE_ITEM_NAME_LENGTH];
-  Position position;
-  Size size;
-} Item;
